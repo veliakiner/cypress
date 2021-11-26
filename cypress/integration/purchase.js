941acbcb4b2e1553c699a88d837a299660e16503
Veli Akiner <veli.akiner@gmail.com>
2021-11-26 13:35:53 +0000

    initial test code

---
+++ cypress/integration/purchase.js
@@ -0,0 +1,11 @@
+import { When, Then } from "cypress-cucumber-preprocessor/steps";
+
+When("I login", () => {
+  cy.loginWith({ email: "user@example.org", password: "this is my password" });
+});
+Then("the url is {word}", (url) => {
+  cy.url().should("eq", `${Cypress.config().baseUrl}${url}`);
+});
+Then("I'm logged", () => {
+  cy.window().its("localStorage.email").should("eq", "user@example.org");
+});
